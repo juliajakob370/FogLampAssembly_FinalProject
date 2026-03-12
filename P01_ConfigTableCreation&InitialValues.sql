8383bb9dcbe8b048e233795f7a7bcc25e731e1d3
Julia Jakob <jjakob3896@conestogac.on.ca>
2026-03-11 21:37:18 -0400

    Added P01 File with the Stored Procedure

--- P01_ConfigTableCreation&InitialValues.sql
+++ P01_ConfigTableCreation&InitialValues.sql
@@ -5,7 +5,7 @@
  * DESCRIPTION   : This script creates the Configuration Table with all of it's properties
  */
 
-DROP DATABASE Jubi; -- drop database if it already exists 
+ DROP DATABASE Jubi; -- drop database if it already exists 
 
  -- == CREATE THE DATABASE == --
  CREATE DATABASE Jubi; -- temporary name for our project database
@@ -25,7 +25,6 @@ DROP DATABASE Jubi; -- drop database if it already exists
 	DataType VARCHAR(20) NULL DEFAULT 'string', -- Data type of value for configuration (optional)
 	Description VARCHAR(255) NULL            -- Description of what the configuration does (optional)
  );
-
 
 
  -------------------------------------------------
@@ -48,9 +47,9 @@ DROP DATABASE Jubi; -- drop database if it already exists
 
  -- Worker Time Configuration Values ----------------------------------------------
  ('BaseTime', '60.0','decimal','Base time for how fast it takes to assemble a completed fog lamp in seconds'),
- ('ExperiencedWorker.TimeVariationPercentage', '10.0','decimal','An experienced worker can assemble a completed fog lamp in the base time +/- this percentage'),
- ('NewEmployee.TimeVariationPercentage', '50.0','decimal','A new worker takes this percentage longer than the base time to complete a fog lamp'),
- ('SuperWorker.TimeVariationPercentage', '15.0','decimal','A super worker can complete a fog lamp in this percentage less time than the base time'),
+ ('ExperiencedWorker.TimeVariant', '10.0','decimal','An experienced worker can assemble a completed fog lamp in the base time +/- this percentage'),
+ ('NewEmployee.TimeVariant', '50.0','decimal','A new worker takes this percentage longer than the base time to complete a fog lamp'),
+ ('SuperWorker.TimeVariant', '15.0','decimal','A super worker can complete a fog lamp in this percentage less time than the base time'),
 
  -- Worker Defect Rates -----------------------------------------------------------
  ('ExperiencedWorker.DefectRate', '0.5','decimal','A normal/experienced worker has this defect percentage'),
@@ -60,3 +59,47 @@ DROP DATABASE Jubi; -- drop database if it already exists
 
  SELECT * FROM Configuration;
 
+ GO
+
+ --===================================================================================--
+ --   CREATE A STORED PROCEDURE SO THAT THE CONFIG TOOL CAN USE IT TO RESET VALUES!   --
+ --===================================================================================--
+ IF OBJECT_ID('dbo.ResetConfigurationToDefaults', 'P') IS NOT NULL
+    DROP PROCEDURE dbo.ResetConfigurationToDefaults; -- drop the procedure if it exists
+ GO
+
+CREATE PROCEDURE dbo.ResetConfigurationToDefaults
+AS
+BEGIN
+    SET NOCOUNT ON; -- so that it doesn't display affected rows
+    DELETE FROM dbo.Configuration; -- delete all data from the config table
+    DBCC CHECKIDENT ('dbo.Configuration', RESEED, 0); -- reset ID count to 0
+    
+    -- re-insert default values!
+	INSERT INTO Configuration (ConfigName, ConfigValue, DataType, Description) VALUES
+	 -- Insert Bin Capacities ------------------------
+	 ('Harness.Capacity', '55', 'int', 'Bin capacity for harnesses'),
+	 ('Reflector.Capacity', '35', 'int', 'Bin capacity for reflectors'),
+	 ('Housing.Capacity', '24', 'int', 'Bin capacity for housings'),
+	 ('Lens.Capacity', '40', 'int', 'Bin capacity for lenses'),
+	 ('Bulb.Capacity', '60', 'int', 'Bin capacity for bulbs'),
+	 ('Bezel.Capacity', '75', 'int', 'Bin capacity for bezels'),
+
+	 -- Factory / Simulation Configurations -------------------------------
+	 ('LowStockThreshold', '5', 'int', 'Number of parts threshold to flag as low quantity'),
+	 ('TotalOrderQuantity', '500', 'int', 'Total number of lamps to build'),
+	 ('NumberOfWorkStations', '3', 'int', 'Number of work stations in the assembly area'),
+	 ('SimulationTimeScale', '1.0', 'decimal', 'Time scale for simulation'),
+
+	 -- Worker Time Configuration Values ----------------------------------------------
+	 ('BaseTime', '60.0','decimal','Base time for how fast it takes to assemble a completed fog lamp in seconds'),
+	 ('ExperiencedWorker.TimeVariant', '10.0','decimal','An experienced worker can assemble a completed fog lamp in the base time +/- this percentage'),
+	 ('NewEmployee.TimeVariant', '50.0','decimal','A new worker takes this percentage longer than the base time to complete a fog lamp'),
+	 ('SuperWorker.TimeVariant', '15.0','decimal','A super worker can complete a fog lamp in this percentage less time than the base time'),
+
+	 -- Worker Defect Rates -----------------------------------------------------------
+	 ('ExperiencedWorker.DefectRate', '0.5','decimal','A normal/experienced worker has this defect percentage'),
+	 ('NewEmployee.DefectRate', '0.85','decimal','A new worker has this defect percentage'),
+	 ('SuperWorker.DefectRate', '0.15','decimal','A super/ very experiencedworker has this defect percentage');
+END;
+GO
